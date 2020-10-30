e58e9f390ab5f3753ae7c042d970a75011f5f026
Almudena Delgado <denaa188@gmail.com>
2020-10-30 17:03:09 +0100

    ADD preguntas 2-6

--- src/trivial.java
+++ src/trivial.java
@@ -1,9 +1,9 @@
-import java.awt.TrayIcon.MessageType;
-
 import javax.swing.JOptionPane;
 
 public class trivial {
-
+	
+	public static final String ANSI_CYAN = "\u001B[36m";
+	
 	public static void main(String[] args) {
 		// CONTADOR PUNTOS
 		// MENSAJE ACIERTO/ERROR POR PREGUNTA
@@ -15,6 +15,7 @@ public class trivial {
 		String srt;
 		String respuesta;
 		
+		//PRIMERA PREGUNTA
 		srt = JOptionPane.showInputDialog("CATEGORIA DE HISTORIA:\n"
 				+ "¿ Quién fue el primer presidente de la democracia española tras el franquismo?") ;
 		respuesta = srt;
@@ -29,6 +30,86 @@ public class trivial {
 			JOptionPane.showMessageDialog(null, "Respuesta incorrecta.\nTus puntos son: " + contPuntos, "ERROR", JOptionPane.ERROR_MESSAGE);
 		}
 		
+		//SEGUNDA PREGUNTA
+		srt = JOptionPane.showInputDialog("CATEGORIA DE GEOGRAFÍA:\n"
+				+ "¿Cuál es la capital de Brasil?") ;
+		respuesta = srt;
+
+		if (respuesta.equalsIgnoreCase("Brasilia") == true) {
+			
+			contPuntos++;
+			JOptionPane.showMessageDialog(null, "Respuesta correcta.\nTus puntos son: " + contPuntos, "BIEN!", JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "Respuesta incorrecta.\nTus puntos son: " + contPuntos, "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		// TERCERA PREGUNTA
+		srt = JOptionPane.showInputDialog("CATEGORIA DE ARTE Y LITERATURA:\n"
+				+ "¿Qué gran artista es conocido por haber pintado la Capilla Sixtina?") ;
+		respuesta = srt;
+
+		if (respuesta.equalsIgnoreCase("Miguel Angel") == true) {
+			
+			contPuntos++;
+			JOptionPane.showMessageDialog(null, "Respuesta correcta.\nTus puntos son: " + contPuntos, "BIEN!", JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "Respuesta incorrecta.\nTus puntos son: " + contPuntos, "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		//CUARTA PREGUNTA  
+		srt = JOptionPane.showInputDialog("CATEGORIA DE ENTRETENIMIENTO:\n"
+				+ "¿Cómo se llama la madre de Simba en la película de Disney “El Rey León”? ") ;
+		respuesta = srt;
+
+		if (respuesta.equalsIgnoreCase("Sarabi") == true) {
+			
+			contPuntos++;
+			JOptionPane.showMessageDialog(null, "Respuesta correcta.\nTus puntos son: " + contPuntos, "BIEN!", JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "Respuesta incorrecta.\nTus puntos son: " + contPuntos, "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+		//QUINTA PREGUNTA
+		srt = JOptionPane.showInputDialog("CATEGORIA DE NATURALEZA Y CIENCIA:\n"
+				+ "¿Cómo se llama la planta a partir de la cual suele ser elaborado el tequila?") ;
+		respuesta = srt;
+
+		if (respuesta.equalsIgnoreCase("Agave") == true) {
+			
+			contPuntos++;
+			JOptionPane.showMessageDialog(null, "Respuesta correcta.\nTus puntos son: " + contPuntos, "BIEN!", JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "Respuesta incorrecta.\nTus puntos son: " + contPuntos, "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+		//SEXTA PREGUNTA
+		srt = JOptionPane.showInputDialog("CATEGORIA DE DEPORTE Y PASATIEMPOS:\n"
+				+ "¿Qué arte marcial de origen sudamericano fue desarrollado para parecerse a un baile?") ;
+		respuesta = srt;
+
+		if (respuesta.equalsIgnoreCase("Capoeira") == true) {
+			
+			contPuntos++;
+			JOptionPane.showMessageDialog(null, "Respuesta correcta.\nTus puntos son: " + contPuntos, "BIEN!", JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "Respuesta incorrecta.\nTus puntos son: " + contPuntos, "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+		//SEPTIMA PREGUNTA
+		
+		//OCTAVA PREGUNTA
+		
+		//NOVENA PREGUNTA
+		
+		//DECIMA PREGUNTA
+
 	}
 
 }
